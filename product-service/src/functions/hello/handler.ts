e69f923b4d4dd50253fad668fb30ab947a25cfd7
Vladimir Kormakov <Vladimir_Kormakov@epam.com>
2021-12-18 22:47:02 +0300

    feat: create basic aws template

---
+++ product-service/src/functions/hello/handler.ts
@@ -0,0 +1,12 @@
+import { formatJSONResponse } from '@libs/apiGateway';
+import { middyfy } from '@libs/lambda';
+
+
+const hello = async (event) => {
+  return formatJSONResponse({
+    message: `Hello, welcome to the exciting Serverless world!`,
+    event,
+  });
+}
+
+export const main = middyfy(hello);
